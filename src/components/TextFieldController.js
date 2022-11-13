c7b0fa581c899f616640235e2ee1de3807572b5a
Andrii Biakov <an.bjakov@gmail.com>
2022-11-13 13:46:04 +0200

    remove control

--- src/components/TextFieldController.js
+++ src/components/TextFieldController.js
@@ -5,7 +5,6 @@ import PropTypes from "prop-types";
 
 const TextFieldController = (props)=>{
     const { field, fieldState } = useController(props);
-    const {control} = useForm()
     return (
         <TextField {...field}
                    label={props.label}
